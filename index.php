286a66491669c29e0ac14b13479a56302071cc4d
Arthur Pereira <arthur.pereira@4selet.com.br>
2025-05-05 15:05:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-
-echo "Hello world!";
-
+<!
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Aula de PHP</title>
+    </head>
+<body>
+    <h1>
+        <?php
+            echo "Hello world!";
+            ?>
+        </h1>
+    </body>
+</html>
